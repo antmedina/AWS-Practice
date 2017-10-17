94fb88000c4c112b6f98ce28d873dc5111938615
antmedina <ant.n.medina@gmail.com>
2017-10-17 14:45:01 -0400

    Test Run

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,7 @@ var express = require("express");
 
 var app = express();
 
-var clientPath = path.join(__dirname,"../client");
+var clientPath = path.join(__dirname, "../client");
 
 app.use(express.static(clientPath));
-app.listen(process.env.PORT||3000);
+app.listen(process.env.PORT || 3000);
